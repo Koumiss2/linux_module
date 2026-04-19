2dd18da56c5f5cb0a47f6d8303f36f5d4f0827a3
anton <anton@example.com>
2026-04-20 00:52:45 +0500

    feat: initialization hook

--- src/icmp.c
+++ src/icmp.c
@@ -3,8 +3,22 @@
 #include <linux/ip.h>
 #include "ping_config.h"
 
-static unsigned int icmp_hook(void *priv, struct sk_buff *skb, const struct nf_hook_state *state);
-static struct nf_hook_ops icmp_ops;
+static unsigned int icmp_hook(void *priv, struct sk_buff *skb, const struct nf_hook_state *state) {
+    struct iphdr *ip;
+
+    iph = ip_hdr(skb);
+    if (!iph) {
+        return NF_ACCEPT;
+    }
+
+    return NF_ACCEPT;
+}
+static struct nf_hook_ops icmp_ops = {
+    .hook     = icmp_hook,
+    .hooknum  = NF_INET_PRE_ROUTING,
+    .pf       = PF_INET,
+    .priority = NF_IP_PRI_FIRST,  
+};
 
 int init_icmp(void) {
     return nf_register_net_hook(&init_net, &icmp_ops);
