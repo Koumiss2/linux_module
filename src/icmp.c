3175344b2b93b722a337ea684999bde548c8dd78
anton <anton@example.com>
2026-04-20 00:47:48 +0500

    feat: signature for icmp hook and register him

--- src/icmp.c
+++ src/icmp.c
@@ -2,3 +2,15 @@
 #include <linux/netfilter_ipv4.h>
 #include <linux/ip.h>
 #include "ping_config.h"
+
+static unsigned int icmp_hook(void *priv, struct sk_buff *skb, const struct nf_hook_state *state);
+static struct nf_hook_ops icmp_ops;
+
+int init_icmp(void) {
+    return nf_register_net_hook(&init_net, &my_nfho);
+}
+
+void deinit_icmp(void)
+{
+    nf_unregister_net_hook(&init_net, &my_nfho);
+}
