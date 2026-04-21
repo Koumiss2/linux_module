102e69876b5f09ecaf120121d056c450c0e9ceff
anton <anton@example.com>
2026-04-21 23:20:19 +0500

    fix: skb dst drop

--- src/netif.c
+++ src/netif.c
@@ -1,24 +1,25 @@
-#include <linux/module.h>
-#include <linux/kernel.h>
-#include <linux/init.h>
 #include <linux/icmp.h>
 #include <linux/if_arp.h>
 #include <linux/if_ether.h>
+#include <linux/netfilter.h>
 #include <linux/netdevice.h>
 #include <linux/skbuff.h>
+#include <linux/string.h>
 #include <linux/ip.h>
-#include <linux/in.h>
+#include <net/dst.h>
 #include "ping_config.h"
 
 static struct net_device *vping_dev;
 
 static int vping_open(struct net_device *dev) {
+    netif_carrier_on(dev);
     netif_start_queue(dev);
     return 0;
 }
 
 static int vping_stop(struct net_device *dev) {
     netif_stop_queue(dev);
+    netif_carrier_off(dev);
     return 0;
 }
 
@@ -30,35 +31,50 @@ static bool vping_make_echo_reply(struct sk_buff *skb, struct net_device *dev) {
     __be32 tmp_addr;
 
     if (!pskb_may_pull(skb, sizeof(struct iphdr)))
+    {
         return false;
+    }
 
     skb_reset_network_header(skb);
     iph = ip_hdr(skb);
 
     if (!iph || iph->version != 4 || iph->protocol != IPPROTO_ICMP)
+    {
         return false;
+    }
 
     ihl = iph->ihl * 4;
     if (ihl < sizeof(struct iphdr))
+    {
         return false;
+    }
 
     if (!pskb_may_pull(skb, ihl + sizeof(struct icmphdr)))
+    {
         return false;
+    }
 
     if (skb_linearize(skb))
+    {
         return false;
+    }
 
     iph = ip_hdr(skb);
-    if (iph->daddr != ip_addr)
+    if (iph->daddr != ip_addr) {
         return false;
+    }
 
     icmph = (struct icmphdr *)((unsigned char *)iph + ihl);
     if (icmph->type != ICMP_ECHO)
+    {
         return false;
+    }
 
     icmp_len = ntohs(iph->tot_len) - ihl;
     if (icmp_len < sizeof(struct icmphdr))
+    {
         return false;
+    }
 
     tmp_addr = iph->saddr;
     iph->saddr = iph->daddr;
@@ -76,6 +92,13 @@ static bool vping_make_echo_reply(struct sk_buff *skb, struct net_device *dev) {
     skb->pkt_type = PACKET_HOST;
     skb->ip_summed = CHECKSUM_NONE;
     skb->skb_iif = dev->ifindex;
+    skb_dst_drop(skb);
+    nf_reset_ct(skb);
+    skb_reset_network_header(skb);
+    skb_set_transport_header(skb, ihl);
+    skb_reset_mac_header(skb);
+    skb_orphan(skb);
+    memset(skb->cb, 0, sizeof(skb->cb));
 
     return true;
 }
@@ -117,13 +140,14 @@ static void vping_setup(struct net_device *dev) {
     dev->type = ARPHRD_NONE;
     dev->hard_header_len = 0;
     dev->addr_len = 0;
-    dev->flags = IFF_NOARP | IFF_POINTOPOINT;
+    dev->flags |= IFF_NOARP | IFF_POINTOPOINT;
     dev->mtu = 1500;
     dev->min_mtu = 68;
     dev->max_mtu = 1500;
     dev->tx_queue_len = 0;
-    dev->features |= NETIF_F_HW_CSUM;
-    dev->needs_free_netdev = false;
+    dev->features |= NETIF_F_HW_CSUM | NETIF_F_NETNS_LOCAL;
+    dev->needed_headroom = 0;
+    dev->needed_tailroom = 0;
 }
 
 int init_netif(void) {
@@ -140,7 +164,6 @@ int init_netif(void) {
         return err;
     }
 
-    pr_info("netif_name: %s\n", vping_dev->name);
     return 0;
 }
 
